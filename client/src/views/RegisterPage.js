74588dcf6b9e3719a73755fd78d8c257249909b7
Thongek Wogponom <thongek.wpm@gmail.com>
2023-09-09 17:57:23 +0700

    commit updata

--- client/src/views/RegisterPage.js
+++ client/src/views/RegisterPage.js
@@ -4,11 +4,16 @@ export default function RegisterPage() {
   const [password, setPassword] = useState("");
   async function register(ev) {
     ev.preventDefault();
-    await fetch("http://localhost:4000/register", {
+    const response = await fetch("http://localhost:4000/register", {
       method: "POST",
       body: JSON.stringify({ username, password }),
       headers: { "Content-Type": "application/json" },
     });
+    if (response.status === 200) {
+      alert("registeration is successful.");
+    } else {
+      alert("registeration is falied.");
+    }
   }
   return (
     <form className="register" onSubmit={register}>
