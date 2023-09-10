3c879d526793caed6989828bebdcb0c710104ac4
Thongek Wogponom <thongek.wpm@gmail.com>
2023-09-10 17:10:25 +0700

    commit updata

--- api/index.js
+++ api/index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const cors = require("cors");
-const User = require("./models/user");
+const User = require("./models/User");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const app = express();
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 const multer = require("multer");
-const uploadMddleware = multer({ dest: "uploads/" });
+const uploadMiddleware = multer({ dest: "uploads/" });
+const fs = require("fs");
+const Post = require("./models/post");
 
 const salt = bcrypt.genSaltSync(10);
 const secret = "asdfe45we45w345wegw345werjktjwertkj";
@@ -65,11 +67,25 @@ app.get("/profile", (req, res) => {
 
 //ส่วนของทำการLogout
 app.post("/logout", (req, res) => {
-  res.cookie("token", "").json("okay");
+  res.cookie("token", "").json("ok");
 });
 
-app.post("/post", uploadMddleware.single("files"), (req, res) => {
-  res.json(req.files);
+app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
+  const { originalname, path } = req.file;
+  const parts = originalname.split(".");
+  const ext = parts[parts.length - 1];
+  const newPath = path + "." + ext;
+  fs.renameSync(path, newPath);
+
+  const { title, summary, content } = req.body;
+  const postDoc = await Post.create({
+    title,
+    summary,
+    content,
+    cover: newPath,
+  });
+
+  res.json(postDoc);
 });
 
 app.listen(4000);
--- client/src/views/CreatePost.js
+++ client/src/views/CreatePost.js
@@ -1,5 +1,6 @@
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import { Navigate } from "react-router-dom";
 import { useState } from "react";
 
 const modules = {
@@ -35,23 +36,33 @@ export default function CreatePost() {
   const [summary, setSummary] = useState("");
   const [content, setContent] = useState("");
   const [files, setFiles] = useState("");
+  const [redirect, setRedirect] = useState(false);
   async function createNewPost(ev) {
     const data = new FormData();
     data.set("title", title);
     data.set("summary", summary);
     data.set("content", content);
-    data.set("files", files);
+    data.set("file", files[0]);
     ev.preventDefault();
     const response = await fetch("http://localhost:4000/post", {
       method: "POST",
       body: data,
+      credentials: "include",
     });
+    if (response.ok) {
+      setRedirect(true);
+    }
   }
+
+  if (redirect) {
+    return <Navigate to={"/"} />;
+  }
+
   return (
     <form onSubmit={createNewPost}>
       <input
         type="title"
-        placeholder={"Title"}
+        placeholder={"title"}
         value={title}
         onChange={(ev) => setTitle(ev.target.value)}
       />
@@ -61,11 +72,7 @@ export default function CreatePost() {
         value={summary}
         onChange={(ev) => setSummary(ev.target.value)}
       />
-      <input
-        type="file"
-        value={files}
-        onChange={(ev) => setFiles(ev.target.value)}
-      />
+      <input type="file" onChange={(ev) => setFiles(ev.target.files)} />
       <ReactQuill
         value={content}
         onChange={(newValue) => setContent(newValue)}
